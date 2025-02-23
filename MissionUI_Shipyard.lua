962630155f0887e2c870cc02d97fc54a88a93cba
l3uGsY <35513633+l3uGsY@users.noreply.github.com>
2025-02-22 22:35:49 -0600

    Reverting

--- MissionUI_Shipyard.lua
+++ MissionUI_Shipyard.lua
@@ -1,4 +1,57 @@
+local addon_name, addon_env = ...
+if not addon_env.load_this then return end
+
+-- Confused about mix of CamelCase and_underscores?
+-- Camel case comes from copypasta of how Blizzard calls returns/fields in their code and deriveates
+-- Underscore are my own variables
+
+-- [AUTOLOCAL START]
+local After = C_Timer.After
+local Enum_GarrisonFollowerType_FollowerType_6_0_Boat = Enum.GarrisonFollowerType.FollowerType_6_0_Boat
+local GARRISON_SHIP_OIL_CURRENCY = GARRISON_SHIP_OIL_CURRENCY
+local GetCurrencyInfo = C_CurrencyInfo.GetCurrencyInfo
+local GetFollowerSoftCap = C_Garrison.GetFollowerSoftCap
+local GetItemInfoInstant = C_Item.GetItemInfoInstant
+local GetNumActiveFollowers = C_Garrison.GetNumActiveFollowers
+local UnitGUID = UnitGUID
+local match = string.match
+local pairs = pairs
+local print = print
+local tinsert = table.insert
+local tsort = table.sort
+local wipe = wipe
+-- [AUTOLOCAL END]
+
+local Widget = addon_env.Widget
+local gmm_buttons = addon_env.gmm_buttons
+local top_for_mission = addon_env.top_for_mission
+local GetFilteredFollowers = addon_env.GetFilteredFollowers
+local UpdateMissionListButton = addon_env.UpdateMissionListButton
+
+local MissionPage = GarrisonShipyardFrame.MissionTab.MissionPage
+
+local function ShipyardMissionList_PartyButtonOnClick(self)
+   if addon_env.RegisterManualInterraction then addon_env.RegisterManualInterraction() end
+   addon_env.mission_page_pending_click = "ShipyardMissionPage1"
+   return self:GetParent():Click()
+end
+
+local shipyard_mission_list_gmm_button_template = {
+   "Button", nil, "UIPanelButtonTemplate",
+   Width = 80, Height = 40, FrameLevelOffset = 3, Scale = 0.60,
+   OnClick = ShipyardMissionList_PartyButtonOnClick,
+}
+local shipyard_mission_list_gmm_loot_template = {
+   "Button", nil, "UIPanelButtonTemplate",
+   Width = 40, Height = 40, FrameLevelOffset = 3, Scale = 0.75,
+   OnClick = ShipyardMissionList_PartyButtonOnClick,
+   Hide = 1,
+}
+
+local loot_frames = {}
+
 local function GarrisonShipyardMap_UpdateMissions_More()
+   -- Blizzard updates those when not visible too, but there's no reason to copy them.
    local self = GarrisonShipyardFrame.MissionTab.MissionList
    if not self:IsVisible() then return end
 
@@ -10,35 +63,60 @@ local function GarrisonShipyardMap_UpdateMissions_More()
       addon_env.top_for_mission_dirty = false
    end
 
-   local filtered_followers = addon_env.GetFilteredFollowers(Enum_GarrisonFollowerType_FollowerType_6_0_Boat)
+   local filtered_followers = GetFilteredFollowers(Enum_GarrisonFollowerType_FollowerType_6_0_Boat)
    local more_missions_to_cache
    local oil = GetCurrencyInfo(GARRISON_SHIP_OIL_CURRENCY).quantity
 
    for i = 1, #missions do
       local mission = missions[i]
+
+      -- Cache mission frames
       local frame = mission_frames[i]
       if frame then
-         if mission.offeredGarrMissionTextureID ~= 0 and not mission.inProgress and not mission.canStart then
+         if (mission.offeredGarrMissionTextureID ~= 0 and not mission.inProgress and not mission.canStart) then
             frame:Hide()
          else
             local gmm_button = gmm_buttons['ShipyardMissionList' .. i]
             if not gmm_button then
-               gmm_button = addon_env.CreateButton('ShipyardMissionList' .. i, frame, i, 80, 40, "TOP", frame, "BOTTOM", 0, 10, false)
-               gmm_button:SetScript("OnClick", ShipyardMissionList_PartyButtonOnClick)
+               shipyard_mission_list_gmm_button_template.parent = frame
+               gmm_button = Widget(shipyard_mission_list_gmm_button_template)
+               gmm_button:SetText(i)
+               gmm_button:SetPoint("TOP", frame, "BOTTOM", 0, 10)
+               gmm_buttons['ShipyardMissionList' .. i] = gmm_button
+
+               shipyard_mission_list_gmm_loot_template.parent = gmm_button
+               local loot_frame = Widget(shipyard_mission_list_gmm_loot_template)
+               loot_frame:SetPoint("LEFT", gmm_button, "RIGHT", -3, 0)
+               loot_frames[i] = loot_frame
             end
 
-            frame.offerSecondsRemaining = mission.offerEndTime and mission.offerEndTime - GetTime()
-            frame.OfferTime:SetText(frame.offerSecondsRemaining and SecondsToTime(frame.offerSecondsRemaining))
-
-            local cant_complete = mission.cost > oil or mission.numFollowers > filtered_followers.free
-            if cant_complete then
-               frame:SetAlpha(0.3)
-               gmm_button:SetText()
+            if (mission.inProgress) then
+               gmm_button:Hide()
             else
-               more_missions_to_cache = addon_env.UpdateMissionListButton(mission, filtered_followers, frame, gmm_button, more_missions_to_cache, oil)
+               gmm_button:Show()
+               more_missions_to_cache = UpdateMissionListButton(mission, filtered_followers, frame, gmm_button, more_missions_to_cache, oil, 0.5)
+               local reward_texture
+               for id, reward in pairs(mission.rewards) do
+                  if reward.itemID then
+                     local _, _, _, _, itemTexture = GetItemInfoInstant(reward.itemID)
+                     reward_texture = itemTexture
+                  elseif reward.currencyID then
+                     local currency_id = reward.currencyID
+                     if currency_id ~= 0 then
+                        local _, _, currencyTexture = GetCurrencyInfo(reward.currencyID);
+                        reward_texture = currencyTexture
+                     end
+                  end
+                  if reward_texture then break end
+               end
+               local loot_frame = loot_frames[i]
+               if reward_texture then
+                  loot_frame:SetNormalTexture(reward_texture)
+                  loot_frame:Show()
+               else
+                  loot_frame:Hide()
+               end
             end
-            gmm_button:Show()
-            frame:Show()
          end
       end
    end
@@ -48,3 +126,55 @@ local function GarrisonShipyardMap_UpdateMissions_More()
    end
 end
 hooksecurefunc("GarrisonShipyardMap_UpdateMissions", GarrisonShipyardMap_UpdateMissions_More)
+
+local function ShipyardInitUI()
+   local prefix = "Shipyard" -- hardcoded, because it is used in OUR frame names and should be static for GMM_Click
+   local follower_type = Enum_GarrisonFollowerType_FollowerType_6_0_Boat
+   local o = addon_env.InitGMMFollowerOptions({
+      follower_type                = follower_type,
+      gmm_prefix                   = prefix,
+      custom_mission_list          = true
+   })
+
+   addon_env.MissionPage_ButtonsInit(follower_type)
+
+   ShipyardInitUI = nil
+end
+ShipyardInitUI()
+
+local BestForCurrentSelectedMission = addon_env.BestForCurrentSelectedMission
+hooksecurefunc(GarrisonShipyardFrame, "ShowMission", function()
+   BestForCurrentSelectedMission(Enum_GarrisonFollowerType_FollowerType_6_0_Boat, MissionPage, "ShipyardMissionPage")
+end)
+
+gmm_buttons.StartShipyardMission = MissionPage.StartMissionButton
+
+local spec_count = {}
+local spec_name = {}
+local spec_list = {}
+-- GossipFrameSharedMixin => GossipFrameMixin
+hooksecurefunc(GossipFrame, "Update", function(...)
+   local guid = UnitGUID("npc")
+   if not (guid and (match(guid, "^Creature%-0%-%d+%-%d+%-%d+%-94429%-") or match(guid, "^Creature%-0%-%d+%-%d+%-%d+%-95002%-"))) then return end
+
+   local filtered_followers = GetFilteredFollowers(Enum_GarrisonFollowerType_FollowerType_6_0_Boat)
+   wipe(spec_count)
+   for idx = 1, #filtered_followers do
+      local follower = filtered_followers[idx]
+      local spec = follower.classSpec
+      local prev_count = spec_count[spec] or 0
+      spec_count[spec] = prev_count + 1
+      spec_name[spec] = follower.className
+   end
+   wipe(spec_list)
+   for spec in pairs(spec_name) do tinsert(spec_list, spec) end
+   tsort(spec_list)
+   for idx = 1, #spec_list do
+      local spec = spec_list[idx]
+      print(spec_name[spec] .. ": " .. spec_count[spec])
+   end
+
+   local max_followers = GetFollowerSoftCap(Enum_GarrisonFollowerType_FollowerType_6_0_Boat)
+   local num_active_followers = GetNumActiveFollowers(Enum_GarrisonFollowerType_FollowerType_6_0_Boat)
+   print(GARRISON_FLEET .. ": " .. num_active_followers .. "/" .. max_followers)
+end)
